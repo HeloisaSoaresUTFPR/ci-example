4c8678a7627da452291689f4cdd70aaf351da065
HeloisaSoaresUtpr <helo_soarescosta@hotmail.com>
2021-03-16 02:08:59 -0300

    BUG-GerarNumeroAleatorio

--- test/utils.test.js
+++ test/utils.test.js
@@ -3,14 +3,28 @@ const { gerarNumeroAleatorio, acharCaracter } = require('../lib/utils');
 describe('Utils', () => {
     describe('gerarNumeroAleatorio', () => {
         test('fim nao pode ser negativo', () => {
-            expect(gerarNumeroAleatorio(20, 5))
+            expect(gerarNumeroAleatorio(20, -5))
                 .toBe(-1);
+        });
+    });
+    
+    describe('gerarNumeroAleatorio', () => {
+        test('Inicio não pode ser marior que fim', () => {
+            expect(gerarNumeroAleatorio(20, 18))
+                .toBe(-1);
+        });
+    });
+
+    describe('gerarNumeroAleatorio', () => {
+        test('Retorna valores', () => {
+            expect(gerarNumeroAleatorio(2, 3))
+                .toBe(3);
         });
     });
 
     describe('acharCaracter', () => {
         test('comprimento invalido', () => {
-            expect(acharCaracter(8, 'abcde', 'c'))
+            expect(acharCaracter(-4, 'abcde', 'c'))
                 .toBe('comprimento invalido');
         });
     });    
